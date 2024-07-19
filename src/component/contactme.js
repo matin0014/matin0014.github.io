97706c96f57deffc31dbdfd044b2470db097c96b
matin0014 <matinraoufi@outlook.com>
2024-07-18 22:23:20 -0700

    Added contact me form

--- src/component/contactme.js
+++ src/component/contactme.js
@@ -1,174 +1,155 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Stack from '@mui/material/Stack';
 import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button'; 
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  components: {
+    MuiFilledInput: {
+      styleOverrides: {
+        root: {
+          backgroundColor: 'black',
+          borderRadius: '4px',
+          border: '2px solid hotpink',
+          '&.Mui-focused': {
+            border: '2px solid hotpink',
+          },
+          '&:after': {
+            borderBottom: '2px solid hotpink',
+          },
+        },
+        input: {
+          color: 'lightgray',
+        },
+      },
+    },
+    MuiInputLabel: {
+      styleOverrides: {
+        root: {
+          color: 'lightgray',
+          '&.Mui-focused': {
+            color: 'lightgray',
+          },
+          '& .MuiFormLabel-asterisk': {
+            display: 'none',
+          },
+        },
+      },
+    },
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          backgroundColor: 'hotpink',
+          color: 'white',
+          '&:hover': {
+            backgroundColor: 'darkmagenta',
+          },
+          borderRadius: '4px',
+          padding: '10px 16px',
+          fontWeight: 'bold',
+          textTransform: 'uppercase',
+        },
+      },
+    },
+  },
+});
 
 export default function FormPropsTextFields() {
+  const [placeholder, setPlaceholder] = React.useState('Enter your email');
+
   return (
-    <Box
-      component="form"
-      sx={{
-        '& .MuiTextField-root': { m: 1, width: '25ch' },
-        color: 'white'
-      }}
-      noValidate
-      autoComplete="off"
-    >
-      <div>
-        <TextField
-          required
-          id="outlined-required"
-          label="Required"
-          defaultValue="Hello World"
-          
-        />
-        <TextField
-          disabled
-          id="outlined-disabled"
-          label="Disabled"
-          defaultValue="Hello World"
-        />
-        <TextField
-          id="outlined-password-input"
-          label="Password"
-          type="password"
-          autoComplete="current-password"
-        />
-        <TextField
-          id="outlined-read-only-input"
-          label="Read Only"
-          defaultValue="Hello World"
-          InputProps={{
-            readOnly: true,
+    <ThemeProvider theme={theme}>
+      <Box
+        id="hero"
+        sx={(theme) => ({
+          width: '100%',
+          backgroundSize: '100% 45%',
+          backgroundRepeat: 'no-repeat',
+        })}
+      >
+        <Container
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center', 
+            pt: { xs: 14, sm: 20 },
+            pb: { xs: 8, sm: 12 },
+            maxWidth: '1200px', 
+            padding: '0 20px', 
           }}
-        />
-        <TextField
-          id="outlined-number"
-          label="Number"
-          type="number"
-          InputLabelProps={{
-            shrink: true,
-          }}
-        />
-        <TextField id="outlined-search" label="Search field" type="search" />
-        <TextField
-          id="outlined-helperText"
-          label="Helper text"
-          defaultValue="Default Value"
-          helperText="Some important text"
-        />
-      </div>
-      <div>
-        <TextField
-          required
-          id="filled-required"
-          label="Required"
-          defaultValue="Hello World"
-          variant="filled"
-        />
-        <TextField
-          disabled
-          id="filled-disabled"
-          label="Disabled"
-          defaultValue="Hello World"
-          variant="filled"
-        />
-        <TextField
-          id="filled-password-input"
-          label="Password"
-          type="password"
-          autoComplete="current-password"
-          variant="filled"
-        />
-        <TextField
-          id="filled-read-only-input"
-          label="Read Only"
-          defaultValue="Hello World"
-          InputProps={{
-            readOnly: true,
-          }}
-          variant="filled"
-        />
-        <TextField
-          id="filled-number"
-          label="Number"
-          type="number"
-          InputLabelProps={{
-            shrink: true,
-          }}
-          variant="filled"
-        />
-        <TextField
-          id="filled-search"
-          label="Search field"
-          type="search"
-          variant="filled"
-        />
-        <TextField
-          id="filled-helperText"
-          label="Helper text"
-          defaultValue="Default Value"
-          helperText="Some important text"
-          variant="filled"
-        />
-      </div>
-      <div>
-        <TextField
-          required
-          id="standard-required"
-          label="Required"
-          defaultValue="Hello World"
-          variant="standard"
-        />
-        <TextField
-          disabled
-          id="standard-disabled"
-          label="Disabled"
-          defaultValue="Hello World"
-          variant="standard"
-        />
-        <TextField
-          id="standard-password-input"
-          label="Password"
-          type="password"
-          autoComplete="current-password"
-          variant="standard"
-        />
-        <TextField
-          id="standard-read-only-input"
-          label="Read Only"
-          defaultValue="Hello World"
-          InputProps={{
-            readOnly: true,
-          }}
-          variant="standard"
-        />
-        <TextField
-          id="standard-number"
-          label="Number"
-          type="number"
-          InputLabelProps={{
-            shrink: true,
-          }}
-          variant="standard"
-        />
-        <TextField
-          id="standard-search"
-          label="Search field"
-          type="search"
-          variant="standard"
-          InputLabelProps={{ style: { color: 'white' } }} // Ensure label text color contrasts
-          InputProps={{ style: { color: 'white' } }} // Ensure input text color contrasts
-        />
-        <TextField
-          id="standard-helperText"
-          label="Helper text"
-          defaultValue="Default Value"
-          helperText="Some important text"
-          variant="standard"
-          InputLabelProps={{ style: { color: 'white' } }} // Ensure label text color contrasts
-          InputProps={{ style: { color: 'white' } }} // Ensure input text color contrasts
-        />
-      </div>
-    </Box>
+        >
+          <Stack spacing={2} sx={{ width: '100%' }}>
+            <Typography
+              variant="h1"
+              sx={{
+                display: 'flex',
+                flexDirection: { xs: 'column', md: 'row' },
+                alignSelf: 'flex-start',
+                textAlign: 'left',
+                fontSize: 'clamp(2.5rem, 9vw, 3rem)',
+                fontWeight: 'bold',
+                color: 'white',
+                mb: 2, 
+              }}
+            >
+              Contact&nbsp;
+              <Typography
+                component="span"
+                variant="h1"
+                sx={{
+                  fontSize: 'clamp(2rem, 9vw, 3rem)',
+                  color: 'hotpink',
+                  fontWeight: 'bold',
+                }}
+              >
+                Me
+              </Typography>
+            </Typography>
+            <Stack spacing={2} direction="row" sx={{ width: '100%' }}>
+              <TextField
+                required
+                id="first-name"
+                label="First Name"
+                variant="filled"
+                sx={{ flex: 1, maxWidth: 'calc(50%)' }}
+              />
+              <TextField
+                required
+                id="last-name"
+                label="Last Name"
+                variant="filled"
+                sx={{ flex: 1, maxWidth: 'calc(50%)' }} 
+              />
+            </Stack>
+            <TextField
+              required
+              id="email"
+              label="Enter your email"
+              variant="filled"
+              sx={{ width: '100%' }} 
+            />
+            <TextField
+              id="message"
+              label="Enter your message"
+              variant="filled"
+              multiline
+              rows={6}
+              sx={{ width: '100%' }} // Make the message field take full width
+            />
+            <Button
+              type="submit"
+              variant="contained"
+              sx={{ width: '100%' }} // Match the width of other fields
+            >
+              Submit
+            </Button>
+          </Stack>
+        </Container>
+      </Box>
+    </ThemeProvider>
   );
 }
